95b06fbb0c73a29aa61187b1f0611c8bfad3ddbd
joel <joel@i-tul.com>
2016-06-29 09:26:25 -0700

    initial commit

---
+++ src/Whateva.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JoelVanPatten\Wonkadoo;
+
+class Whateva
+{
+    /**
+     * @param  string $adjective
+     * @return string
+     */
+    public function doSomething($adjective)
+    {
+        return 'This is '.$adjective;
+    }
+}
